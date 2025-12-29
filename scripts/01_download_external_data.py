a20c2607b4738fae2625c8c20e976a10468be8fd
Manuel Rueda <rueda.manu@gmail.com>
2025-12-29 17:16:41 +0100

    Source commit

--- scripts/01_download_external_data.py
+++ scripts/01_download_external_data.py
@@ -2,13 +2,13 @@ import os, gdown
 
 # Dictionary mapping the desired output filenames to their Google Drive file IDs
 files = {
-    "data.tar.gz.md5":      "1KPQdljTv-jDk8kodRgNGcajby97chva_",
-    "data.tar.gz.part-00":  "1xNX0vH5Ya4XqhQEylk_ZZNVtpEeCsZD9",
-    "data.tar.gz.part-01":  "11sJTFojJkgUyrKMROAQihP4K9z8515Kt",
-    "data.tar.gz.part-02":  "1jkUMU0iyDMeZ_jkad_24yk1YcTcYghM2",
-    "data.tar.gz.part-03":  "1U2iZ1kzQ5vwbj3FuTcMJ2Vl2Oxegg0C3",
-    "data.tar.gz.part-04":  "1zgCjShM0T6fOAMEirbQdoLT03V7-1RxR",
-    "data.tar.gz.part-05":  "1faW5N8NECugJddJyoTfpn5rtkb1tNygl",
+    "data.tar.gz.md5": "1MmeppkZ1xR9ODsBLuDWUh-2ob98Prxq6",
+    "data.tar.gz.part-00": "12HQw_duxmlcASh7Yw8yL2-f-hyjqMim9",
+    "data.tar.gz.part-01": "1Ejrn1oQ2WO3TSTmWSfoQ1IjYBOXivbRm",
+    "data.tar.gz.part-02": "18CC1y2t5heV66-jOHCFIHqgj9GbT3QgF",
+    "data.tar.gz.part-03": "1mq5t0U8GFk6ZdaMqBzWHD73LfYJ626vi",
+    "data.tar.gz.part-04": "1v8rQA-qtgYgnPcOV8PzO-otw9dE9ePqt",
+    "data.tar.gz.part-05": "1knI_DEdrlushYj5lOclnRNDdvSEmXQ5m",
 }
 
 def download_if_missing(filename, file_id):
